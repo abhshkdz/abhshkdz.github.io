cb33e1d1796a5fd78834dddf62e4a746586b552c
Abhishek Das <das.abhshk@gmail.com>
2013-09-01 17:39:14 +0530

    Creates a webhook for posting to partychat

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,16 @@
 //custom js goes here
+$(document).ready(function(){
+
+	var body = window.location.search.replace( "?", "" ).split('=')[1];
+	if (body != '' && body != undefined)
+	{
+		$.ajax({
+			type: 'POST',
+			url: 'http://partychat-hooks.appspot.com/post/p_ztixmmrm',
+			data: {
+				body: unescape(body)
+			}
+		})
+	}
+
+});
