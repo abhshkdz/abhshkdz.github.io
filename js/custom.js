e41c639e07b1faeedd61b9053706d3c15c9326f1
Abhishek Das <das.abhshk@gmail.com>
2013-09-09 03:16:06 +0530

    Adds G+ comments

--- js/custom.js
+++ js/custom.js
@@ -13,4 +13,15 @@ $(document).ready(function(){
 		})
 	}
 
+	
+
+	$( document ).ready(function() {
+		gapi.comments.render('google_comments', {
+			href: window.location,
+			width: $('article.post').width(),
+			first_party_property: 'BLOGGER',
+			view_type: 'FILTERED_POSTMOD'
+		});
+	});
+
 });
